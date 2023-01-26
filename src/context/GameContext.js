0250840ccd7c52f5e9930a5d10d64f6c62aba720
Alejae1998 <alejagr1998@gmail.com>
2023-01-26 11:01:19 -0800

    initialize state

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -4,14 +4,21 @@ const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [board, setBoard] = useState([
-    { space: 1, content: '' }
-    { space: 2, content: '' }
-    { space: 3, content: '' }
-    { space: 4, content: '' }
-    { space: 5, content: '' }
-    { space: 6, content: '' }
-    { space: 7, content: '' }
-    { space: 8, content: '' }
-    { space: 9, content: '' }
-]);
+    { space: 1, content: '' },
+    { space: 2, content: '' },
+    { space: 3, content: '' },
+    { space: 4, content: '' },
+    { space: 5, content: '' },
+    { space: 6, content: '' },
+    { space: 7, content: '' },
+    { space: 8, content: '' },
+    { space: 9, content: '' },
+  ]);
+  const [currentPlayer, setCurrentPlayer] = useState('O');
+  const [active, setActive] = useState(false);
+  const [gameMessage, setGameMessage] = useState('');
+
+  return <GameContext.Provider value={{}}>{children}</GameContext.Provider>;
 };
+
+export { GameProvider };
