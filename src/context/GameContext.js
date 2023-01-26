798a0b1ba9b32b0b1fa6f53065a0ae1586592abc
Alejae1998 <alejagr1998@gmail.com>
2023-01-26 10:57:03 -0800

    set up gamecontext initialize my board state following the todo list

---
+++ src/context/GameContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [board, setBoard] = useState([
+    { space: 1, content: '' }
+    { space: 2, content: '' }
+    { space: 3, content: '' }
+    { space: 4, content: '' }
+    { space: 5, content: '' }
+    { space: 6, content: '' }
+    { space: 7, content: '' }
+    { space: 8, content: '' }
+    { space: 9, content: '' }
+]);
+};
